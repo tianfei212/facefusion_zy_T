43d8a2bb8ba6aa93edfb76a68f486a0be2e74219
drunkenQCat <songjh123123@outlook.com>
2025-06-29 15:54:37 +0800

    使用uv管理版本，去掉了一些与中影无关的东西

--- facefusion/metadata.py
+++ facefusion/metadata.py
@@ -1,17 +1,16 @@
 from typing import Optional
 
-METADATA =\
-{
-	'name': 'FaceFusion',
-	'description': 'Industry leading face manipulation platform',
-	'version': '3.3.0',
-	'license': 'OpenRAIL-AS',
-	'author': 'Henry Ruhs',
-	'url': 'https://facefusion.io'
+METADATA = {
+    "name": "中影人工智能研究院",
+    "description": "Industry leading face manipulation platform",
+    "version": "3.3.0",
+    "license": "OpenRAIL-AS",
+    "author": "中影人工智能研究院",
+    "url": "https://facefusion.io",
 }
 
 
-def get(key : str) -> Optional[str]:
-	if key in METADATA:
-		return METADATA.get(key)
-	return None
+def get(key: str) -> Optional[str]:
+    if key in METADATA:
+        return METADATA.get(key)
+    return None
